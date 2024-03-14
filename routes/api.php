ee3b6a4187e51b5fcc50734e4b91d3e103787692
Andrew Sharanda <azzimandias@gmail.com>
2024-03-14 11:10:24 +0300

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -26,24 +26,6 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 
 });
-Route::middleware(['auth:sanctum'])->group(function () {
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
-Route::get('/', function () { return view('welcome'); });
-Route::get('/dev/create', [DevController::class, 'devCreate']);
-Route::get('/tasks', [TaskController::class, 'tasks']);
-Route::get('/lists', [PersonalListController::class, 'lists']);
-Route::get('/sortLists', [PersonalListController::class, 'sortLists']);
-Route::get('/tags', [TagController::class, 'tags']);
-Route::get('/list', [PersonalListController::class, 'personalListTasks']);
-Route::get('/tag', [TagController::class, 'taggedTasks']);
-Route::get('/user', [UserController::class, 'users']);
-Route::post('/updateTask', [TaskController::class, 'updateTask']);
-Route::post('/createTask', [TaskController::class, 'createTask']);
-Route::post('/deleteTask', [TaskController::class, 'deleteTask']);
-Route::post('/saveList', [PersonalListController::class, 'saveList']);
-Route::post('/deleteList', [PersonalListController::class, 'deleteList']);
-Route::post('/globalSearch', [TaskController::class, 'globalSearch']);
-// require __DIR__.'/auth.php';
-});
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,25 @@ use Illuminate\Support\Facades\Route;
 
 
 
+
+
+
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
 Route::get('/', function () { return view('welcome'); });
+Route::get('/dev/create', [DevController::class, 'devCreate']);
+Route::get('/tasks', [TaskController::class, 'tasks'])->middleware(['auth:sanctum']);
+Route::get('/lists', [PersonalListController::class, 'lists']);
+Route::get('/sortLists', [PersonalListController::class, 'sortLists']);
+Route::get('/tags', [TagController::class, 'tags']);
+Route::get('/list', [PersonalListController::class, 'personalListTasks']);
+Route::get('/tag', [TagController::class, 'taggedTasks']);
+Route::get('/user', [UserController::class, 'users']);
+Route::post('/updateTask', [TaskController::class, 'updateTask']);
+Route::post('/createTask', [TaskController::class, 'createTask']);
+Route::post('/deleteTask', [TaskController::class, 'deleteTask']);
+Route::post('/saveList', [PersonalListController::class, 'saveList']);
+Route::post('/deleteList', [PersonalListController::class, 'deleteList']);
+Route::post('/globalSearch', [TaskController::class, 'globalSearch']);
+require __DIR__.'/auth.php';
