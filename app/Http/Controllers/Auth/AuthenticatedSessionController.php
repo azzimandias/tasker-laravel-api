45c7dbaf06aadf35cf189bc162275ebea403b339
Andrew Sharanda <35540190+azzimandias@users.noreply.github.com>
2024-11-21 16:01:16 +0300

    login auth

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -19,7 +19,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return response()->noContent();
+        return response('done', 200);
     }
 
     /**
