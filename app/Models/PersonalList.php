c11376100d8ffd72859564f19998b07feca7a95b
Andrew Sharanda <35540190+azzimandias@users.noreply.github.com>
2025-09-05 21:44:00 +0300

    lil fix

--- app/Models/PersonalList.php
+++ app/Models/PersonalList.php
@@ -23,7 +23,8 @@ class PersonalList extends Model
     public function users()
     {
         return $this->belongsToMany(User::class, 'user_list', 'list_id', 'user_id')
-            ->wherePivotNull('deleted_at');
+            ->wherePivotNull('deleted_at')
+            ->whereNull('users.deleted_at');
     }
 
     public function tasks()
